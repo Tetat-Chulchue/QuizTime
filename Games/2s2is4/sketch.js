114b448735d96e0739ed7d66828141dda8512505
KISSADAPA <42942414+KISSADAPA@users.noreply.github.com>
2019-10-16 11:33:14 +0700

    Update sketch.js
    End Game

--- Games/2s2is4/sketch.js
+++ Games/2s2is4/sketch.js
@@ -1,6 +1,6 @@
 // ------------- Declare Variable ------------
-let question;
-let questionKey;
+//let question;
+//let questionKey;
 let questionObj;
 let questionIndex;
 let life;
@@ -49,36 +49,47 @@ function draw() {
 
     background(155);
 
+    if (life === 0) {
+        button_1.remove();
+        button_2.remove();
+        button_3.remove();
+        button_4.remove();
+
+        textSize(60);
+        text("Congratulations!", windowWidth / 2, (windowHeight / 2) - 100);
+        text("Your score is : " + score, windowWidth / 2, (windowHeight / 2) + 100);
+    } else {
+        textSize(40);
+        text("Life : " + life, 90, 50);
+        text("Score : " + score, windowWidth - 120, 50);
+
+        textSize(60);
+        text(question, windowWidth / 2, (windowHeight / 2) / 2);
+    }
+
     button_1.position(0, windowHeight / 2);
     button_1.style('background-color', '#ff4d4d');
-    button_1.style('font-size', '40px');
+    button_1.style('font-size', '55px');
     button_1.style('font-weight', 'bold');
     button_1.size(windowWidth / 2, (windowHeight / 2) / 2);
 
     button_2.position(windowWidth / 2, windowHeight / 2);
     button_2.style('background-color', '#00ff00');
-    button_2.style('font-size', '40px');
+    button_2.style('font-size', '55px');
     button_2.style('font-weight', 'bold');
     button_2.size(windowWidth / 2, (windowHeight / 2) / 2);
 
     button_3.position(0, (windowHeight * 1.5) / 2);
     button_3.style('background-color', '#ffff00');
-    button_3.style('font-size', '40px');
+    button_3.style('font-size', '55px');
     button_3.style('font-weight', 'bold');
     button_3.size(windowWidth / 2, (windowHeight / 2) / 2);
 
     button_4.position(windowWidth / 2, (windowHeight * 1.5) / 2);
     button_4.style('background-color', '#6666ff');
-    button_4.style('font-size', '40px');
+    button_4.style('font-size', '55px');
     button_4.style('font-weight', 'bold');
     button_4.size(windowWidth / 2, (windowHeight / 2) / 2);
-
-    textSize(40);
-    text("Life : " + life, 90, 50);
-    text("Score : " + score, windowWidth - 120, 50);
-
-    textSize(60);
-    text(question, windowWidth / 2, (windowHeight / 2) / 2);
 }
 // -------------------------------------------
 
