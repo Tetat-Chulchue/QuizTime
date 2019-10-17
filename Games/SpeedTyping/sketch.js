8451022a77b02afe4c9c1e3340f0f2981ef653d6
Tetat Chulchue <tetat.chulchue@gmail.com>
2019-10-17 12:32:19 +0700

    Add endGame function

--- Games/SpeedTyping/sketch.js
+++ Games/SpeedTyping/sketch.js
@@ -1,10 +1,13 @@
-let txt;
-let paragraph;
-let paragraphIndex = 0;
-let isLetter = /^[a-z]$/;
-let charArray;
-let timer = 1;
-let timerStatus = true;
+// ------------ Declare Variables --------------
+
+let txt;                    // paragraph loaded fram p5
+let paragraph;              // paragraph string
+let paragraphIndex = 0;     // current index  of paragraph
+let charArray;              // array of span
+let timer = 1;              // current time
+let end = false           // current game state
+
+// -------------- P5's Functions --------------
 
 function preload() {
     txt = loadStrings('./test.txt');
@@ -17,7 +20,7 @@ function draw() {
     text('Time: ', width / 2, height / 2);
     textAlign(LEFT, CENTER)
     text(timer, width / 2, height / 2);
-    if ((frameCount % 60 == 0 && timer > 0) && timerStatus === true) {
+    if ((frameCount % 60 == 0 && timer > 0) && !end === true) {
         timer++;
         console.log(timer);
     }
@@ -31,6 +34,22 @@ function setup() {
     write();
 }
 
+function keyPressed() {
+    if (!end) {
+        if (key.toLowerCase() === charArray[paragraphIndex].innerHTML) {
+            charArray[paragraphIndex].id = 'correct';
+        } else {
+            charArray[paragraphIndex].id = 'wrong';
+        }
+    }
+    paragraphIndex++;
+    if (paragraphIndex + 1 === paragraph.length) {
+        endGame();
+    }
+}
+
+// ------------- My Functions -----------
+
 function write() {
     let main = document.createElement('p');
     for (let i = 0; i < paragraph.length; i++) {
@@ -42,21 +61,7 @@ function write() {
     document.body.appendChild(main);
 }
 
-function keyPressed() {
-    if (key.toLowerCase() === charArray[paragraphIndex].innerHTML) {
-        charArray[paragraphIndex].id = 'correct';
-    } else if (key === ENTER){
-        timerStatus = false;
-        wordPerMinute();
-    } else {
-        charArray[paragraphIndex].id = 'wrong';
-    }
-    paragraphIndex++;
+function endGame() {
+    end = true;
+    console.log(0);
 }
-
-// function wordPerMinute() {
-//     let str = paragraph.toString();
-//     let numWord = str.split(" ");
-//     let countWord = numWord.length;
-//     console.log(countWord)
-// }
