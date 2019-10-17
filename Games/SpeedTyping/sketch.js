fe8bd3394ff6e99de7e2f2507f0c24bc57d72cb2
Unknown <61070008@kmitl.ac.th>
2019-10-17 12:17:46 +0700

    Update
    Mother Fking Timer

--- Games/SpeedTyping/sketch.js
+++ Games/SpeedTyping/sketch.js
@@ -3,12 +3,28 @@ let paragraph;
 let paragraphIndex = 0;
 let isLetter = /^[a-z]$/;
 let charArray;
+let timer = 1;
+let timerStatus = true;
 
 function preload() {
     txt = loadStrings('./test.txt');
 }
 
+function draw() {
+    background(999);
+    textAlign(RIGHT, CENTER);
+    textSize(30);
+    text('Time: ', width / 2, height / 2);
+    textAlign(LEFT, CENTER)
+    text(timer, width / 2, height / 2);
+    if ((frameCount % 60 == 0 && timer > 0) && timerStatus === true) {
+        timer++;
+        console.log(timer);
+    }
+}
+
 function setup() {
+    createCanvas(200, 60);
     paragraph = txt[0].toLowerCase();
     console.log(paragraph);
     charArray = document.getElementsByTagName('span');
@@ -29,8 +45,18 @@ function write() {
 function keyPressed() {
     if (key.toLowerCase() === charArray[paragraphIndex].innerHTML) {
         charArray[paragraphIndex].id = 'correct';
+    } else if (key === ENTER){
+        timerStatus = false;
+        wordPerMinute();
     } else {
         charArray[paragraphIndex].id = 'wrong';
     }
     paragraphIndex++;
 }
+
+// function wordPerMinute() {
+//     let str = paragraph.toString();
+//     let numWord = str.split(" ");
+//     let countWord = numWord.length;
+//     console.log(countWord)
+// }
