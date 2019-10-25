4b456933eb042c83cacbf46ce01282c85c986d40
Teerasak <61070094@kmitl.ac.th>
2019-10-25 10:50:42 +0700

    Update Vocab bug
    #prototype NOT TO USE!!!!

--- Web/Game1/vocab/script/animal.js
+++ Web/Game1/vocab/script/animal.js
@@ -17,7 +17,9 @@ function setup() {
         let row = table.insertRow(-1);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
-        cell1.innerHTML = vocabularyKeys[index];
-        cell2.innerHTML = vocabularyValues[index];
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = index+1;
+        cell2.innerHTML = vocabularyKeys[index];
+        cell3.innerHTML = vocabularyValues[index];
     }
 }
