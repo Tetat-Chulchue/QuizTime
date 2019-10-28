050f4d5bc98eb19c2fcaeee1fde5e847ff5b9883
Teerasak <61070094@kmitl.ac.th>
2019-10-28 21:43:43 +0700

    Update Vocab design by: Na_OMEGA v.2
    +New design web vocab prototype(animal.html) V.2

--- Web/Game1/vocab/script/time.js
+++ Web/Game1/vocab/script/time.js
@@ -9,15 +9,16 @@ function preload() {
 function setup() {
     vocabularyKeys = Object.keys(vocabularyObj);
     vocabularyValues = Object.values(vocabularyObj);
-    console.log(vocabularyKeys)
-    console.log(vocabularyValues)
-    console.log(vocabularyObj)
     let table = document.getElementById("mytable");
+    let tbody = document.createElement('tbody');
     for (let index = 0; index < vocabularyKeys.length; index++) {
-        let row = table.insertRow(-1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        cell1.innerHTML = vocabularyKeys[index];
-        cell2.innerHTML = vocabularyValues[index];
+        let row = tbody.insertRow(-1);
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+        cell0.innerHTML = `<p>${index + 1}</p>`;
+        cell1.innerHTML = `<p>${vocabularyKeys[index]}</p>`;
+        cell2.innerHTML = `<p>${vocabularyValues[index]}</p>`;
+        table.append(tbody);
     }
 }
