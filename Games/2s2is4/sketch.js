03ae4a310879bfbfdf1f75a670e84ba49e472164
KISSADAPA <42942414+KISSADAPA@users.noreply.github.com>
2019-11-05 13:42:44 +0700

    update

--- Games/2s2is4/sketch.js
+++ Games/2s2is4/sketch.js
@@ -3,7 +3,7 @@ let question;
 let questionKey;
 let questionObj;
 let questionIndex;
-let life = 3;
+let life = 4;
 let score = 0;
 let correctSound;
 let wrongSound;
@@ -27,7 +27,8 @@ function setup() {
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
 
-    if (life === 3) {
+    if (life === 4) {
+        life = 3;
         questionKey = Object.keys(questionObj);
     }
 
@@ -35,18 +36,22 @@ function setup() {
 
     button_1 = createButton(questionObj[question][0].choice[0], '0');
     button_1.id('button_1');
+    button_1.class('button');
     button_1.mousePressed(check);
 
     button_2 = createButton(questionObj[question][0].choice[1], '1');
     button_2.id('button_2');
+    button_2.class('button');
     button_2.mousePressed(check);
 
     button_3 = createButton(questionObj[question][0].choice[2], '2');
     button_3.id('button_3');
+    button_3.class('button');
     button_3.mousePressed(check);
 
     button_4 = createButton(questionObj[question][0].choice[3], '3');
     button_4.id('button_4');
+    button_4.class('button');
     button_4.mousePressed(check);
 
     fill(255,255,255);
@@ -82,35 +87,15 @@ function draw() {
     }
 
     button_1.position(15, windowHeight / 2);
-    button_1.style('background-color', '#f0134d');
-    button_1.style('color', '#ffffff');
-    button_1.style('font-size', '55px');
-    button_1.style('font-family', 'btn_Kanit-ExtraLight');
-    button_1.style('border', 'none');
     button_1.size((windowWidth / 2) - 25, ((windowHeight / 2) / 2) - 15);
 
     button_2.position((windowWidth / 2) + 10, windowHeight / 2);
-    button_2.style('background-color', '#3e64ff');
-    button_2.style('color', '#ffffff');
-    button_2.style('font-size', '55px');
-    button_2.style('font-family', 'btn_Kanit-ExtraLight');
-    button_2.style('border', 'none');
     button_2.size((windowWidth / 2) - 25, ((windowHeight / 2) / 2) - 15);
 
     button_3.position(15, (windowHeight * 1.5) / 2);
-    button_3.style('background-color', '#ffd369');
-    button_3.style('color', '#ffffff');
-    button_3.style('font-size', '55px');
-    button_3.style('font-family', 'btn_Kanit-ExtraLight');
-    button_3.style('border', 'none');
     button_3.size((windowWidth / 2) - 25, ((windowHeight / 2) / 2) - 15);
 
     button_4.position((windowWidth / 2) + 10, (windowHeight * 1.5) / 2);
-    button_4.style('background-color', '#5eb7b7');
-    button_4.style('color', '#ffffff');
-    button_4.style('font-size', '55px');
-    button_4.style('font-family', 'btn_Kanit-ExtraLight');
-    button_4.style('border', 'none');
     button_4.size((windowWidth / 2) - 25, ((windowHeight / 2) / 2) - 15);
 }
 // -------------------------------------------
@@ -122,7 +107,14 @@ function randomQuestion() {
 }
 
 function check() {
-    if (questionObj[question][0].choice[0] === questionObj[question][1].check) {
+    let num;
+
+    document.getElementById("button_1").click(num = this.value());
+    document.getElementById("button_2").click(num = this.value());
+    document.getElementById("button_3").click(num = this.value());
+    document.getElementById("button_4").click(num = this.value());
+    
+    if (questionObj[question][0].choice[num] === questionObj[question][1].check) {
         score += 10;
         correctSound.play();
     } else {
@@ -130,43 +122,6 @@ function check() {
         wrongSound.play();
     }
 
-    del();
-}
-
-function check_1() {
-    if (questionObj[question][0].choice[1] === questionObj[question][1].check) {
-        score += 10;
-        correctSound.play();
-    } else {
-        life -= 1;
-         wrongSound.play();
-    }
-    del();
-}
-
-function check_2() {
-    if (questionObj[question][0].choice[2] === questionObj[question][1].check) {
-        score += 10;
-        correctSound.play();
-    } else {
-        life -= 1;
-        wrongSound.play();
-    }
-    del();
-}
-
-function check_3() {
-    if (questionObj[question][0].choice[3] === questionObj[question][1].check) {
-        score += 10;
-        correctSound.play();
-    } else {
-        life -= 1;
-        wrongSound.play();
-    }
-    del();
-}
-
-function del() {
     questionKey.splice(questionIndex, 1);
     button_1.remove();
     button_2.remove();
