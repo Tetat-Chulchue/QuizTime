d6cf5f562831d8f3ce7311e2b85846906ad720ed
61070078 <61070078@kmitl.ac.th>
2019-11-14 18:32:40 +0700

    Fix loading

--- Web/JS/main_page/loading.js
+++ Web/JS/main_page/loading.js
@@ -3,16 +3,19 @@ window.addEventListener("load", (test) => {
     let span_2 = document.getElementById("span_2");
     let loader_img = document.getElementById("loader_img");
     let pace = document.getElementById("pace_lib");
+    let loadder = document.getElementById("loadder");
+
     span_1.style.animationName = "span_1";
     span_2.style.animationName = "span_2";
     loader_img.style.animationName = "img";
     pace.style.animationName = "pace";
     pace.style.display = "initial";
     setTimeout(()=>{
-        let loadder = document.getElementById("loadder");
         document.body.style.overflow = "initial";
         loader_img.style.display = "none";
         pace.style.display = "none";
-        loadder.className += " hidden"
+    }, 2000);
+    setTimeout(() => {
+        loadder.className += " hidden";
     }, 2000);
 });
