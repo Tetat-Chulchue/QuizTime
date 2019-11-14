8a79d5f1f904864014e17070efde3e4040b63d39
61070078 <61070078@kmitl.ac.th>
2019-11-14 18:01:34 +0700

    Update new loading

--- Web/JS/main_page/loading.js
+++ Web/JS/main_page/loading.js
@@ -2,12 +2,17 @@ window.addEventListener("load", (test) => {
     let span_1 = document.getElementById("span_1");
     let span_2 = document.getElementById("span_2");
     let loader_img = document.getElementById("loader_img");
+    let pace = document.getElementById("pace_lib");
     span_1.style.animationName = "span_1";
     span_2.style.animationName = "span_2";
-    loader_img.style.display = "none";
+    loader_img.style.animationName = "img";
+    pace.style.animationName = "pace";
+    pace.style.display = "initial";
     setTimeout(()=>{
         let loadder = document.getElementById("loadder");
         loadder.className += " hidden"
         document.body.style.overflow = "initial";
+        loader_img.style.display = "none";
+        pace.style.display = "none";
     }, 2000);
 });
