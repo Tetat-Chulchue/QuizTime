94ff7d53c9b1c2b760a96819b6ef0b37b22f7fa9
61070078 <61070078@kmitl.ac.th>
2019-11-14 17:31:25 +0700

    New loading

--- Web/JS/main_page/loading.js
+++ Web/JS/main_page/loading.js
@@ -1,8 +1,13 @@
 window.addEventListener("load", (test) => {
-    console.log(test);
+    let span_1 = document.getElementById("span_1");
+    let span_2 = document.getElementById("span_2");
+    let loader_img = document.getElementById("loader_img");
+    span_1.style.animationName = "span_1";
+    span_2.style.animationName = "span_2";
+    loader_img.style.display = "none";
     setTimeout(()=>{
         let loadder = document.getElementById("loadder");
         loadder.className += " hidden"
         document.body.style.overflow = "initial";
-    }, 1000);
+    }, 2000);
 });
